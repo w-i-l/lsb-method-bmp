187f8fe4cc9b0c33020cd0cd57d5c800b42999de
Ocnaru Mihai <masterpaper01@gmail.com>
2023-03-23 17:13:55 +0200

    increased buffer size
    need to optimize and clear

--- main.c
+++ main.c
@@ -4,10 +4,12 @@
 #include "bmp.h"
 
 
-#define size 456
+#define size 37500000
 
 
-void pattern1(FILE* f, unsigned int no_of_blocks){
+void pattern1(const char* filename, unsigned int no_of_blocks){
+
+    FILE* f = fopen(filename, "ab");
 
     BGR* first = BGR_init(0,0,255);
     BGR* second = BGR_init(0,255,0);
@@ -28,10 +30,15 @@ void pattern1(FILE* f, unsigned int no_of_blocks){
         }
     }
 
+    fclose(f);
+
 }
 
 
-void pattern2(FILE* f, unsigned int no_of_blocks){
+void pattern2(const char* filename, unsigned int no_of_blocks){
+
+    FILE* f = fopen(filename, "ab");
+
     BGR *colors[1000];
     int index = 0;
 
@@ -48,12 +55,16 @@ void pattern2(FILE* f, unsigned int no_of_blocks){
             BGR_write_to_file(f, colors[i * j % 1000]);
         }
     }
+
+    fclose(f);
 }
 
 
-void lsb_method_write(FILE* from, FILE* to, char* message){
+void lsb_method_write(const char* from_file, const char* to_file, char* message){
 
     FILE* tmp = fopen("tmp.txt", "w");
+    FILE* from = fopen(from_file , "rb");
+    FILE* to = fopen(to_file, "wb");
 
     fseek(from, 0xa, SEEK_SET);
     int offset;
@@ -72,6 +83,7 @@ void lsb_method_write(FILE* from, FILE* to, char* message){
     fwrite(buffer, sizeof(char), 54, to);
 
     unsigned int index = 0, no_of_bits = 8;
+    int final_no_of_bits = 8;
 
     printf("%i %i %i\n", offset, width, height);
 
@@ -83,6 +95,7 @@ void lsb_method_write(FILE* from, FILE* to, char* message){
             fread(&aux, sizeof(BGR), 1, from);
             // printf("%c", message[index]);
             if(message[index] != 0){
+
                 if(no_of_bits >= 3){
                     bit = ((message[index] & (1 << (no_of_bits - 1) )) >> (no_of_bits - 1));
                     aux.b = ((aux.b >> 1) << 1) | bit;
@@ -118,18 +131,44 @@ void lsb_method_write(FILE* from, FILE* to, char* message){
                     aux.r = ((aux.r >> 1) << 1) | ((message[index] & (1 << 6)) >> 6);
                     no_of_bits = 6;
                 }
-                // fprintf(tmp, "%c %c %c\n", (aux.b & 1) + '0', (aux.g & 1) + '0', (aux.r & 1) + '0');
+
             }
+            // if we end to write the message
+            // we write 0xff
+            else if(final_no_of_bits > -20){
+
+                printf("Apelat %d", final_no_of_bits);
+                
+                aux.b = ((aux.b >> 1) << 1) | 1;
+                aux.g = ((aux.g >> 1) << 1) | 1;
+                aux.r = ((aux.r >> 1) << 1) | 1;
+
+                final_no_of_bits -= 3;
+            }
+            
+            fprintf(tmp, "%c %c %c\n", (aux.b & 1) + '0', (aux.g & 1) + '0', (aux.r & 1) + '0');
             fwrite(&aux, sizeof(BGR), 1, to);
         }
     }
     fclose(tmp);
+    fclose(from);
+    fclose(to);
 
 }
 
 
-void lsb_method_read(FILE* from){
+void clear_buffer(char* buffer){
+    int i=0;
 
+    for(i=0; i<size; i++)
+        buffer[i] = 0;
+}
+
+
+void lsb_method_read(const char* from_file, const char* to_file){
+
+    FILE* from = fopen(from_file , "rb");
+    FILE* to = fopen(to_file, "wb");
     FILE* tmp = fopen("tmp2.txt", "w");
 
     fseek(from, 0xa, SEEK_SET);
@@ -145,23 +184,22 @@ void lsb_method_read(FILE* from){
     char c[54];
     fread(c, sizeof(char), 54, from);
 
-    // fwrite(buffer, sizeof(char), 54, to); 
-    // printf("%d %d %d %ld\n", offset, width, height, ftell(from));
+    char* buffer = (char*) malloc(sizeof(char) * size);
 
-    // const int size = 100;
-    char buffer[size+1] = {0}, current_char;
+    if(buffer == NULL){
+        printf("Problem with memory allocation!\n");
+        exit(-1);
+    }
+    
     int buffer_index = 0, no_of_bits = 0;
 
     BGR aux;
 
-    // printf("debug\n");
 
     for(int i=0; i<width; i++){
         for(int j=0; j<height; j++){
 
-
             fread(&aux, sizeof(BGR), 1, from);
-            // printf("%ld\n", ftell(from));
 
             if(no_of_bits <= 5){
 
@@ -175,103 +213,126 @@ void lsb_method_read(FILE* from){
                 buffer[buffer_index] |= aux.r & 1;
 
                 no_of_bits += 3;
+                
+                if(buffer[buffer_index] == 127){
+                    fwrite(buffer, sizeof(char), buffer_index-1, to);  
+                    printf("Done\n");
+                    return;
+                }
 
                 if(buffer_index != size-1){
                     if(no_of_bits == 8){
                         buffer_index ++;
                         buffer[buffer_index] = 0;
+                        // clear_buffer(buffer);
                         no_of_bits = 0;
                     }
                 }
                 else{
-                    buffer[buffer_index+1] = 0;
-                    printf("%s", buffer);
+                    fwrite(buffer, sizeof(char), size, to);  
+                    fprintf(tmp, "Printed 1\n");              
                     buffer_index = 0;
-                    buffer[buffer_index] = 0;
+                    clear_buffer(buffer);
                     no_of_bits = 0;
                 }
             }
             else if(no_of_bits == 6){
+
+                if(buffer[buffer_index] == 127){
+                    fwrite(buffer, sizeof(char), buffer_index-1, to);  
+                    printf("Done\n");
+                    return;
+                }
+
                 buffer[buffer_index] <<= 1;
                 buffer[buffer_index] |= aux.b & 1;
 
                 buffer[buffer_index] <<= 1;
                 buffer[buffer_index] |= aux.g & 1;
 
+
                 if(buffer_index != size-1){
-                        buffer_index ++;
+                    buffer_index ++;
+                    buffer[buffer_index] = 0;
                 }
                 else{
-                    buffer[buffer_index + 1] = 0;
-                    printf("%s", buffer);
+                    fwrite(buffer, sizeof(char), size, to);      
+                    fprintf(tmp, "Printed 2\n");              
                     buffer_index = 0;
+                    clear_buffer(buffer);
                 }
 
-                buffer[buffer_index] = 0;
-
-
-                buffer[buffer_index] <<= 1;
                 buffer[buffer_index] |= aux.r & 1;
 
                 no_of_bits = 1;
             }
             else if(no_of_bits == 7){
+
+                if(buffer[buffer_index] == 127){
+                    fwrite(buffer, sizeof(char), buffer_index-1, to);  
+                    printf("Done\n");
+                    return;
+                }
+
                 buffer[buffer_index] <<= 1;
                 buffer[buffer_index] |= aux.b & 1;
 
                 if(buffer_index != size-1){
                     buffer_index ++;
+                    buffer[buffer_index] = 0;
                 }
                 else{
-                    buffer[buffer_index + 1] = 0;
-                    printf("%s", buffer);
+                    fwrite(buffer, sizeof(char), size, to);     
+                    fprintf(tmp, "Printed 3\n");              
                     buffer_index = 0;
+                    clear_buffer(buffer);
                 }
-                buffer[buffer_index] = 0;
-
                 
-                buffer[buffer_index] <<= 1;
                 buffer[buffer_index] |= aux.g & 1;
 
                 buffer[buffer_index] <<= 1;
                 buffer[buffer_index] |= aux.r & 1;
-
                 no_of_bits = 2;
             }
 
-            fprintf(tmp, "%d %d %d\n", aux.b & 1, aux.g & 1, aux.r & 1);
         }
-    }
+    }              
+
+    // if there is limited space
+    fwrite(buffer, sizeof(char), buffer_index, to);  
+
     fclose(tmp);
+    fclose(from);
+    fclose(to);
 }
 
 
 int main(){
     
-    FILE* f = fopen("img.bmp", "r+b");
-    // FILE* file = fopen("img.bmp", "wb");
-    FILE* g = fopen("out.bmp", "r+b");
-    FILE* h = fopen("pointillist.bmp", "r+b");
+    // FILE* f = fopen("img .bmp", "r+b");
+    // // FILE* file = fopen("img. bmp", "wb");
+    // FILE* g = fopen("out .bmp", "r+b");
+    // FILE* h = fopen("pointillist .bmp", "r+b");
+    // FILE* decoded = fopen("decoded .bin", "w+b");
 
-    unsigned int no_of_blocks = 1000;
+    unsigned int no_of_blocks = 10;
     unsigned int data_size = no_of_blocks * no_of_blocks * 3;
 
-    char message[] = "am fost la ana si cornel sa aflu si eu ce s-a mai vandut am fost la ana si cornel sa aflu si eu ce s-a mai vandut am fost la ana si cornel sa aflu si eu ce s-a mai vandut am fost la ana si cornel sa aflu si eu ce s-a mai vandutam fost la ana si cornel sa aflu si eu ce s-a mai vandut am fost la ana si cornel sa aflu si eu ce s-a mai vandut am fost la ana si cornel sa aflu si eu ce s-a mai vandut am fost la ana si cornel sa aflu si eu ce s-a mai vandut";
+    char message[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n";
 
+    BMP_header* h1 = BMP_header_init(data_size);
+    DIB_header* d1 = DIB_header_init(no_of_blocks, no_of_blocks, data_size);
 
-    // lsb_method_write(f, g, message);
-    lsb_method_read(g);
+    BMP_header_write_to_file("test.bmp", h1);
+    DIB_header_write_to_file("test.bmp", d1);
+    
+    pattern2("test.bmp", no_of_blocks);
+
+    lsb_method_write("test.bmp", "test2.bmp", message);
+    lsb_method_read("test2.bmp", "decoded.bin");
 
 
     // clock_t start = clock();
-
-    // BMP_header* h1 = BMP_header_init(data_size);
-    // DIB_header* d1 = DIB_header_init(no_of_blocks, no_of_blocks, data_size);
-
-    // BMP_header_write_to_file(f, h1);
-    // DIB_header_write_to_file(f, d1);
-    
-    // pattern1(f, no_of_blocks);
 
     
 
